e1355cb31d78011fd0637136451a517d5d50890b
Abinaya M <abinayamanokaran2006@gmail.com>
2026-07-07 07:42:21 +0530

    Add frontend login page with API integration

--- js/api.js
+++ js/api.js
@@ -1,16 +1,24 @@
-function getToken() {
-  return localStorage.getItem("access_token");
+const API_BASE_URL = "https://fashion-ecommerce-2.onrender.com";
+
+const API = {
+    LOGIN: `${API_BASE_URL}/api/accounts/login`,
+    REGISTER: `${API_BASE_URL}/api/accounts/register`,
+    PRODUCTS: `${API_BASE_URL}/api/products`,
+    CART: `${API_BASE_URL}/api/cart`,
+    WISHLIST: `${API_BASE_URL}/api/wishlist`,
+    PROFILE: `${API_BASE_URL}/api/accounts/profile`,
+    ORDERS: `${API_BASE_URL}/api/orders`,
+    BANNERS: `${API_BASE_URL}/api/banners/active`
+};
+function normalHeaders() {
+    return {
+        "Content-Type": "application/json"
+    };
 }
 
 function authHeaders() {
-  return {
-    "Content-Type": "application/json",
-    Authorization: "Bearer " + getToken(),
-  };
-}
-
-function normalHeaders() {
-  return {
-    "Content-Type": "application/json",
-  };
+    return {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + localStorage.getItem("access_token")
+    };
 }
