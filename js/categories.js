802aed0a0dd575f93bb56758396ce952ce0f5c52
Abinaya M <abinayamanokaran2006@gmail.com>
2026-07-15 00:30:52 +0530

    feat: add dynamic shop by category section

---
+++ js/categories.js
@@ -0,0 +1,58 @@
+console.log(API);
+console.log(API.CATEGORIES);
+
+async function loadCategories() {
+
+    try {
+
+        const response = await fetch(API.CATEGORIES);
+
+        const result = await response.json();
+
+        const categories = result.data;
+
+        const container = document.getElementById("categoryContainer");
+
+        container.innerHTML = "";
+
+        categories.forEach(category => {
+
+            container.innerHTML += `
+
+                <div class="col-lg-3 col-md-6 col-sm-6">
+
+                    <div class="category-card">
+
+                        <img src="${category.image}" alt="${category.name}">
+
+                        <div class="category-content">
+
+                            <h4>${category.name}</h4>
+
+                            <a href="pages/products.html?category=${category.id}" class="shop-btn">
+
+                                Shop Now
+
+                            </a>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            `;
+
+        });
+
+    }
+
+    catch(error){
+
+        console.error("Category Error:", error);
+
+    }
+
+}
+
+loadCategories();
--- js/config.js
+++ js/config.js
@@ -1,24 +1,32 @@
 const BASE_URL = "https://fashion-ecommerce-ozav.onrender.com";
 
-
 const API = {
-    LOGIN: `${BASE_URL}/api/accounts/login`,
-    REGISTER: `${BASE_URL}/api/accounts/register`,
-    PRODUCTS: `${BASE_URL}/api/products`,
-    CART: `${BASE_URL}/api/cart`,
-    WISHLIST: `${BASE_URL}/api/wishlist`,
-    PROFILE: `${BASE_URL}/api/accounts/profile`,
-    ORDERS: `${BASE_URL}/api/orders`,
-    BANNERS: `${BASE_URL}/api/banners/active`
-};
 
+    LOGIN: `${BASE_URL}/api/accounts/login`,
+
+    REGISTER: `${BASE_URL}/api/accounts/register`,
+
+    PRODUCTS: `${BASE_URL}/api/products`,
+
+    CART: `${BASE_URL}/api/cart`,
+
+    WISHLIST: `${BASE_URL}/api/wishlist`,
+
+    PROFILE: `${BASE_URL}/api/accounts/profile`,
+
+    ORDERS: `${BASE_URL}/api/orders`,
+
+    BANNERS: `${BASE_URL}/api/banners`,
+
+    CATEGORIES: `${BASE_URL}/api/categories`
+
+};
 
 function normalHeaders() {
     return {
         "Content-Type": "application/json"
     };
 }
-
 
 function authHeaders() {
     return {
